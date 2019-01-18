bcaed8e19fcf94df217e572fea4dbefe27608693
Pratik Pandya <pratikpandya@Pratiks-Mac-mini.local>
2019-01-18 14:33:03 +0530

    Initial Commit

---
+++ GoogleMapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMapDemo
+//
+//  Created by Pratik Pandya on 18/01/19.
+//  Copyright © 2019 Pratik Pandya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
